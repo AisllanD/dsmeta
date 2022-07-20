8516a18e33a9615a50d63ebe6eb0b90a2ec6a1e5
Aisllan <aisllangba82@gmail.com>
2022-07-19 21:45:25 -0300

    update redes sociais

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import logo from "../../assets/img/logo.svg";
+import logoGithub from "../../assets/img/icon_Github.svg";
+import logoLinkedin from "../../assets/img/iconLinkedin.svg"
 import './styles.css'
 
 function Header() {
@@ -9,10 +11,18 @@ function Header() {
           <img src={logo} alt="DSMeta" />
           <h1>DSMeta</h1>
           <p>
-            Desenvolvido por:  <blockquote></blockquote>  
+            Desenvolvido por: Aisllan
+          </p>
+          <p className="links">
+
             <a href="https://github.com/AisllanD" target="_blank">
-              @AisllanD.github
+              <img  src={logoGithub} alt="github" />
             </a>
+
+            <a href="https://www.linkedin.com/in/aisllan-bezerra-993ba11a4/" target="_blank">
+              <img src={logoLinkedin} alt="linkedin" />
+            </a>
+
           </p>
         </div>
       </header>
